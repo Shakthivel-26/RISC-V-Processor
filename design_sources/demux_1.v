27def584bcc7fb1f807f2b5a820821d113864ac8
Shakthivel <shakthivelsachin56@gmail.com>
2025-11-18 12:19:15 +0530

    create demux_1

--- design_sources/demux_1.v
+++ design_sources/demux_1.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 03.10.2025 16:08:38
+// Create Date: 03.10.2025 16:03:10
 // Design Name: 
-// Module Name: mux_3
+// Module Name: demux_1
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,15 +20,12 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module mux_3 (
- input wire [31:0] mux_3_in0,
- input wire [31:0] mux_3_in1,
- input wire [31:0] mux_3_in2,
- input wire [1:0] mux_3_sel,
- output wire [31:0] mux_3_out
+module demux_1 (
+ input wire [31:0] demux_in,
+ input wire demux_sel,
+ output wire [31:0] demux_out0,
+ output wire [31:0] demux_out1
  );
- assign mux_3_out = (mux_3_sel == 2'b00)? mux_3_in0 :
- (mux_3_sel == 2'b01)? mux_3_in1 :
- (mux_3_sel == 2'b10)?
- mux_3_in2 : 32'b0;
+ assign demux_out0 = demux_sel? 32'b0: demux_in;
+ assign demux_out1 = demux_sel? demux_in: 32'b0;
 endmodule
